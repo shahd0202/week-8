0f989b35f293ba9a79507a228d431dabd4685fd1
shahd0202 <85609674+shahd0202@users.noreply.github.com>
2021-06-09 09:47:25 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var getusername = prompt("Hello! who are you? please state your name:");
+
+console. log(getusername);
+
+document.getElementById("username"). innerText = getusername;
+
+//tooltip
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
